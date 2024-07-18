a4732276fe9093ba7a5d7417a951762720dcffac
Arjun_Sharma <ssh.00000008@gmail.com>
2024-07-18 11:07:09 +0545

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+if (isset($_FILES['file'])) {
+    $uploadDir = '/uploads';
+    $uploadFile = $uploadDir . basename($_FILES['file']['name']);
+    if (!is_dir($uploadDir)) {
+        mkdir($uploadDir, 0777, true);
+    }
+    if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadFile)) {
+        echo "File was successfully uploaded.";
+    } else {
+        echo "File upload failed.";
+    }
+} else {
+    echo "<h1>Lisning....</h1>";
+}
+?
